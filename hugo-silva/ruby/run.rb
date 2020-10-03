9cbcc5e12ea3cc5d2ebe3f7d7aab236cc6aa5f90
repl.it user <replituser@example.com>
2020-10-03 02:30:15 +0000

    add run file

---
+++ hugo-silva/ruby/run.rb
@@ -0,0 +1,9 @@
+require_relative 'bubble_sort.rb'
+require_relative 'factorial.rb'
+require_relative 'linear_search.rb'
+
+BubbleSort.new.sort([2,3,6,4,65])
+
+Factorial.new.calcule(3)
+
+LinearSearch.new.search([1,3,5,6,3,9], 6)
